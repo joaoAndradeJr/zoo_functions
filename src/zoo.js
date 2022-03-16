8fa49d061a880895366c8c210a34255ff551ade5
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-03-16 13:16:56 -0400

    feat: addEmployee function

--- src/zoo.js
+++ src/zoo.js
@@ -45,8 +45,8 @@ function isManager(id) {
   return result.some((e) => e !== undefined);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
